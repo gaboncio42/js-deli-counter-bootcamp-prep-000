687c1d7f6ff7d83ed8bd5cf1ff3f5999e08cbf82
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-21 23:05:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
+
+function nowServing(katzDeliLine) {
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    i++;
+  } if (katzDeliLine === 0) {
+    return "There is nobody waiting to be served!";
+  } else { 
+  return (`Currently serving ${katzDeliLine.shift()}.`); 
+  }
+}
+
+
+
